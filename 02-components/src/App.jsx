689e029f138021eed25717a59c1bbf6a69346ce1
sahilrihadh <sahilrihadh@gmail.com>
2025-11-07 17:11:48 +0530

    Initial Vite React project upload

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+
+export const App = () => {
+  return (
+    <>
+    
+    <Navbar />
+    <Hero />
+    
+    </>
+  )
+}
+
+export default App 
---
+++ 03-Props/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+      <img src="https://picsum.photos/400/300" alt="" />
+      <h4>Card Title</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, est.</p>
+      <button className='btn btn-priamry'>Read More</button>
+    </div>
+  )
+}
+
+export default Card
